3f2ab049c09db22aa504f3d3e67e376801042e8f
Sarthak Rohatgi <infinitybusiness726@gmail.com>
2022-09-25 01:27:07 +0530

    ticket id fix

--- src/models/students.js
+++ src/models/students.js
@@ -1,48 +1,39 @@
-
-const mongoose = require('mongoose');
-const validator = require('validator');
-//definig schema 
+const mongoose = require("mongoose");
+const validator = require("validator");
+//definig schema
 const studentSchema = new mongoose.Schema({
-Firstname : {
-    type : String,
-    required : true,
-    minlength : 3,
+  Firstname: {
+    type: String,
+    required: true,
+    minlength: 3,
+  },
+  Lastname: {
+    type: String,
+    required: true,
+    minlength: 3,
+  },
 
-},
-Lastname : {
-    type : String,
-    required : true,
-    minlength : 3,
+  email: {
+    type: String,
+    required: true,
 
-},
-
-    email : {
-        type : String,
-        required : true,
-     
-        unique : [true , "Email id is already present"],
-        validate(value){
-            if(!validator.isEmail(value)){
-                throw new Error("Invalid Email");
-            }
-           
-
-        }
+    unique: [true, "Email id is already present"],
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Invalid Email");
+      }
     },
-    Ticket_id : {
-        type : Number,
-        min : 4 ,
+  },
+  Ticket_id: {
+    type: Number,
+    min: 4,
 
-       unique : true 
-    } ,
-   Date : {
-         type : Date,
-         default : Date.now()
-
-        }})
+    unique: true,
+  }
+},  {
+    timestamps: true,
+    versionKey: false, // Here You have to add.
+});
 // we will create a new collection  or a model
-const Student = new mongoose.model('Student',studentSchema);
+const Student = new mongoose.model("Student", studentSchema);
 module.exports = Student;
-
-
-
